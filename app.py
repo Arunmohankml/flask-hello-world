82360c3c961c9a603db91c31654ea9bcdb766534
Arunmohankml <arunmohankrl@gmail.com>
2025-05-26 18:24:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask
+import eventlet
+eventlet.monkey_patch()
+
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, async_mode='eventlet')  # Explicitly use eventlet
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+@socketio.on('message')
+def handle_message(msg):
+    print(f'Message: {msg}')
+    send(msg, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
